3767e0f51d483d4b9e1b565a026bc6472419e942
Vyacheslav Rusakov <vyarus@gmail.com>
2015-12-05 17:10:21 +0600

    add publication override test

---
+++ src/test/groovy/ru/vyarus/gradle/plugin/lib/PublicationOverrideTest.groovy
@@ -0,0 +1,43 @@
+package ru.vyarus.gradle.plugin.lib
+
+import org.gradle.testkit.runner.TaskOutcome
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 05.12.2015
+ */
+class PublicationOverrideTest extends AbstractKitTest {
+
+    def "Check install task"() {
+        setup:
+        file('src/main/java').mkdirs()
+        build """
+            plugins {
+                id 'java'
+                id 'ru.vyarus.java-lib'
+            }
+
+            group 'ru.vyarus'
+            version 1.0
+
+            publishing.publications.maven.artifacts = [jar, javadocJar]
+        """
+
+        when: "run pom task"
+        def result = run('install')
+
+
+        String artifactId = projectName()
+        File deploy = file("build/repo/ru/vyarus/$artifactId/1.0/")
+
+        then: "task done"
+        result.task(":install").outcome == TaskOutcome.SUCCESS
+        result.standardOutput.contains("INSTALLED ru.vyarus:$artifactId:1.0")
+
+        then: "artifacts deployed"
+        deploy.exists()
+        def baseName = artifactId + '-1.0'
+        deploy.list() as Set ==
+                ["${baseName}.jar", "${baseName}.pom", "${baseName}-javadoc.jar"] as Set<String>
+    }
+}
