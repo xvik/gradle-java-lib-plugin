5d36120798c7a86bc23cdbff7a37d791fec104eb
Vyacheslav Rusakov <vyarus@gmail.com>
2019-11-09 23:58:04 +0700

    fix travis and java11 builds

--- src/test/groovy/ru/vyarus/gradle/plugin/lib/LegacyModeKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/lib/LegacyModeKitTest.groovy
@@ -1,11 +1,13 @@
 package ru.vyarus.gradle.plugin.lib
 
 import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.IgnoreIf
 
 /**
  * @author Vyacheslav Rusakov
  * @since 11.07.2018
  */
+@IgnoreIf({jvm.java9Compatible}) // gradle 4.6 will not work on java 11
 class LegacyModeKitTest extends AbstractKitTest {
 
     def "Check install task"() {
